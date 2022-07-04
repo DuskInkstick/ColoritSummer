ea6d666f4fff75f5d07587ad2c051f7222b6a445
StyxHelix <lightnight19@yandex.ru>
2022-07-04 16:45:47 +0400

    Добавлены базовые интерфейсы

---
+++ ColoritSummer.Interfaces/Entities/INamedEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ColoritSummer.Interfaces.Entities
+{
+    public  interface INamedEntity : IEntity
+    {
+        [Required]
+        string Name { get; }
+    }
+}
---
+++ ColoritSummer.Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using ColoritSummer.Interfaces.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ColoritSummer.Interfaces.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task<int> GetCount(CancellationToken cancel = default);
+
+        Task<IEnumerable<T>> GetAll(CancellationToken cancel = default);
+
+        Task<IEnumerable<T>> Get(int skip, int count, CancellationToken cancel = default);
+
+        Task<T> GetById(int id, CancellationToken cancel = default);
+
+        Task<T> Add(T item, CancellationToken cancel = default);
+
+        Task<T> Update(T item, CancellationToken cancel = default);
+
+        Task<T> Delete(T item, CancellationToken cancel = default);
+
+        Task<T> DeleteById(int id, CancellationToken cancel = default);
+    }
+}
