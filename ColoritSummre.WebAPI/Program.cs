e03204502124738e39e74dbe2abc2962f9d2f339
StyxHelix <lightnight19@yandex.ru>
2022-07-08 15:30:21 +0400

    Вернул .NET6 стиль конфигурации WebAPI приложения

--- ColoritSummre.WebAPI/Program.cs
+++ ColoritSummre.WebAPI/Program.cs
@@ -1,16 +1,43 @@
-using ColoritSummer.WebAPI;
+using ColoritSummer.Data.MySQL.Context;
+using ColoritSummer.Data.MySQL.Repositories;
+using ColoritSummer.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("MySQLDatabase");
+builder.Services.AddDbContext<ColoritSummerDbContext>(opt =>
+opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(DbRepository<>));
+builder.Services.AddScoped(typeof(IUserRepository<>), typeof(DbUserRepository<>));
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
